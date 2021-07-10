9c4c0980ec3d878262f122fd5864f216fe3c2580
okada genya <g.okada.wilf+github@gmail.com>
2021-07-11 00:05:07 +0900

    a

--- src/GoogleCalendar.tsx
+++ src/GoogleCalendar.tsx
@@ -32,88 +32,88 @@ const initClient = () => {
   })
 }
 
-// /**
-//  *  Called when the signed in status changes, to update the UI
-//  *  appropriately. After a sign-in, the API is called.
-//  */
-// function updateSigninStatus(isSignedIn: boolean) {
-//   if (isSignedIn) {
-//     // @ts-ignore 
-//     authorizeButton.style.display = 'none'
-//     // @ts-ignore 
-//     signoutButton.style.display = 'block'
-//     listUpcomingEvents()
-//   } else {
-//     // @ts-ignore 
-//     authorizeButton.style.display = 'block'
-//     // @ts-ignore 
-//     signoutButton.style.display = 'none'
-//   }
-// }
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn: boolean) {
+  if (isSignedIn) {
+    // @ts-ignore 
+    authorizeButton.style.display = 'none'
+    // @ts-ignore 
+    signoutButton.style.display = 'block'
+    listUpcomingEvents()
+  } else {
+    // @ts-ignore 
+    authorizeButton.style.display = 'block'
+    // @ts-ignore 
+    signoutButton.style.display = 'none'
+  }
+}
 
-// /**
-//  *  Sign in the user upon button click.
-//  */
-// function handleAuthClick() {
-//   // @ts-ignore 
-//   gapi.auth2.getAuthInstance().signIn()
-// }
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick() {
+  // @ts-ignore 
+  gapi.auth2.getAuthInstance().signIn()
+}
 
-// /**
-//  *  Sign out the user upon button click.
-//  */
-// function handleSignoutClick() {
-//   // @ts-ignore 
-//   gapi.auth2.getAuthInstance().signOut()
-// }
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick() {
+  // @ts-ignore 
+  gapi.auth2.getAuthInstance().signOut()
+}
 
-// /**
-//  * Append a pre element to the body containing the given message
-//  * as its text node. Used to display the results of the API call.
-//  *
-//  * @param {string} message Text to be placed in pre element.
-//  */
-// function appendPre(message: string) {
-//   var pre = document.getElementById('content')
-//   var textContent = document.createTextNode(message + '\n')
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node. Used to display the results of the API call.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message: string) {
+  var pre = document.getElementById('content')
+  var textContent = document.createTextNode(message + '\n')
 
-//   // @ts-ignore 
-//   pre.appendChild(textContent)
-// }
+  // @ts-ignore 
+  pre.appendChild(textContent)
+}
 
-// /**
-//  * Print the summary and start datetime/date of the next ten events in
-//  * the authorized user's calendar. If no events are found an
-//  * appropriate message is printed.
-//  */
-// function listUpcomingEvents() {
-//   // @ts-ignore 
-//   gapi.client.calendar.events.list({
-//     'calendarId': 'primary',
-//     'timeMin': (new Date()).toISOString(),
-//     'showDeleted': false,
-//     'singleEvents': true,
-//     'maxResults': 10,
-//     'orderBy': 'startTime'
-//     // @ts-ignore 
-//   }).then(function(response) {
-//     var events = response.result.items
-//     appendPre('Upcoming events:')
+/**
+ * Print the summary and start datetime/date of the next ten events in
+ * the authorized user's calendar. If no events are found an
+ * appropriate message is printed.
+ */
+function listUpcomingEvents() {
+  // @ts-ignore 
+  gapi.client.calendar.events.list({
+    'calendarId': 'primary',
+    'timeMin': (new Date()).toISOString(),
+    'showDeleted': false,
+    'singleEvents': true,
+    'maxResults': 10,
+    'orderBy': 'startTime'
+    // @ts-ignore 
+  }).then(function(response) {
+    var events = response.result.items
+    appendPre('Upcoming events:')
 
-//     if (events.length > 0) {
-//       for (let i = 0 i < events.length i++) {
-//         var event = events[i]
-//         var when = event.start.dateTime
-//         if (!when) {
-//           when = event.start.date
-//         }
-//         appendPre(event.summary + ' (' + when + ')')
-//       }
-//     } else {
-//       appendPre('No upcoming events found.')
-//     }
-//   })
-// }
+    if (events.length > 0) {
+      for (let i = 0 i < events.length i++) {
+        var event = events[i]
+        var when = event.start.dateTime
+        if (!when) {
+          when = event.start.date
+        }
+        appendPre(event.summary + ' (' + when + ')')
+      }
+    } else {
+      appendPre('No upcoming events found.')
+    }
+  })
+}
 
 
 
